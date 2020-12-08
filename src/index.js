adc5ad90ad65dc80b0b94dfc59a9d680c317fba2
tsarapoid <nadein@gmail.com>
2020-12-08 23:53:56 +0300

    add useRef

--- src/index.js
+++ src/index.js
@@ -1,24 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 
-const endpoint = 'https://api.github.com/users/codeartistryio'
+const baseUrl = 'https://api.github.com/users/'
 
 const App = () => {
-  
+  const [userName, setUserName] = useState('codeartistryio')
   const [user, setUser] = useState({})
+  const searchInput = useRef()
+  
+  useEffect(() => {
+    getUser()
+  }, [])
+  
+  function handleClearInput(){
+    searchInput.current.value = ''
+    searchInput.current.focus()
+  }
+
+  async function getUser() {
+    const response = await fetch(`${baseUrl}${userName}`)
+    const data = await response.json()
+    setUser(data)
+    /* console.log(route) */
+  }
 
   useEffect(() => {
-    async function getUser() {
-    const response = await fetch(endpoint)
-    const data = await response.json()
-    setUser(data)}
     getUser()
   }, [])
 
 
   return (
     <div>
+      <input
+        type='text'
+        placeholder='Input username'
+        onChange = {event => setUserName(event.target.value)}
+        ref = {searchInput}
+      />
+      <button
+        onClick={getUser}>
+        Search
+      </button>
+      <button
+        onClick = {handleClearInput}>
+        Clear
+      </button>
       <h2>{user.name}</h2>
       <p>{user.bio}</p>
       <img alt='avatar' src={user.avatar_url} style={{height: 300}}/>
